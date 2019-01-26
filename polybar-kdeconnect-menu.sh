0b2e1886ce1b51537a72055ab8cc6d4436128d88
Haider Ali Punjabi <haiderali@cyberservices.com>
2019-01-26 09:31:32 +0530

    Replaced dmenu string with device name

--- polybar-kdeconnect-menu.sh
+++ polybar-kdeconnect-menu.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 device="a48f193c969ca8dc"
+devicename="$(kdeconnect-cli -a | grep $device | awk -F'[-|:]' '{print $2}')"
 isavail="$(kdeconnect-cli -a | grep $device | grep "paired and reachable")"
 battery="$(qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$device org.kde.kdeconnect.device.battery.charge)"
 if ! [ -z $isavail ]
 then
-    MENU="$(rofi -sep "|" -dmenu -i -p 'System'  -width 15 -hide-scrollbar -theme material -line-padding 4 -padding 20 -lines 4 -font "Fantasque Sans Mono 10" <<< "Battery: $battery%|Ping|Find Device|Send File")"
+    MENU="$(rofi -sep "|" -dmenu -i -p $devicename -width 15 -hide-scrollbar -theme material -line-padding 4 -padding 20 -lines 4 -font "Fantasque Sans Mono 10" <<< "Battery: $battery%|Ping|Find Device|Send File")"
                 case "$MENU" in
                     *Ping) kdeconnect-cli --ping -d $device ;;
                     *"Find Device") kdeconnect-cli --ring -d $device ;;
