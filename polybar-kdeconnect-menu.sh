fb7fdf85cd74bf0db1268af530dfd3721d4aa9e0
Haider Ali Punjabi <haiderali@cyberservices.com>
2019-01-26 10:58:40 +0530

    Cleaned up code

--- polybar-kdeconnect-menu.sh
+++ polybar-kdeconnect-menu.sh
@@ -1,18 +1,13 @@
-#!/bin/sh
-device="a48f193c969ca8dc"
-devicename="$(kdeconnect-cli -a | grep $device | awk -F'[-|:]' '{print $2}')"
-isavail="$(kdeconnect-cli -a | grep $device | grep "paired and reachable")"
-battery="$(qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$device org.kde.kdeconnect.device.battery.charge)"
+#!/usr/bin/env bash
+
+. $(dirname "$0")/polybar-kdeconnect.config
+
 if ! [ -z $isavail ]
 then
-    MENU="$(rofi -sep "|" -dmenu -i -p $devicename -width 15 -hide-scrollbar -theme material -line-padding 4 -padding 20 -lines 4 -font "Fantasque Sans Mono 10" <<< "Battery: $battery%|Ping|Find Device|Send File")"
-                case "$MENU" in
+    menu="$(rofi -sep "|" -dmenu -i -p $devicename -location $LOCATION -yoffset $YOFFSET -xoffset $XOFFSET -theme $THEME -width $WIDTH -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< "Battery: $battery%|Ping|Find Device|Send File")"
+                case "$menu" in
                     *Ping) kdeconnect-cli --ping -d $device ;;
-                    *"Find Device") kdeconnect-cli --ring -d $device ;;
-                    *"Send File") kdeconnect-cli --share "$(zenity --file-selection)" -d $device
-                    # *Lock) i3lock-fancy ;;
-                    # *Logout) openbox --exit;;
-                    # *Reboot) systemctl reboot ;;
-                    # *Shutdown) systemctl -i poweroff
+                    *'Find Device') kdeconnect-cli --ring -d $device ;;
+                    *'Send File') kdeconnect-cli --share "$(zenity --file-selection)" -d $device
                 esac
 fi
--- polybar-kdeconnect.sh
+++ polybar-kdeconnect.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
-ICON_CONNECTED=""
-ICON_DISCONNECTED=""
-device="a48f193c969ca8dc"
-isavail="$(kdeconnect-cli -a | grep $device | grep "paired and reachable")"
-battery="$(qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$device org.kde.kdeconnect.device.battery.charge)"
+#!/usr/bin/env bash
+
+. $(dirname "$0")/polybar-kdeconnect.config
+
 if [ -z "$isavail" ]
 then
     echo $ICON_DISCONNECTED
